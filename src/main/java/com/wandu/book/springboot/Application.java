6659ae7fa5039b1e031b12ef6a783df419ab3260
Wandu12 <info10229@gmail.com>
2020-10-07 02:58:31 +0900

    edit test code

--- src/main/java/com/wandu/book/springboot/Application.java
+++ src/main/java/com/wandu/book/springboot/Application.java
@@ -2,9 +2,8 @@ package com.wandu.book.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing //JPA Auditing 활성화
+//@EnableJpaAuditing //JPA Auditing 비활성화(따로 분리)
 // @SpringBootApplication이 있는 위치부터 설정을 읽어가므로, 이 클래스는 항상 프로젝트 최상단에 위치해야 함.
 @SpringBootApplication
 public class Application {
--- src/test/java/com/wandu/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/wandu/book/springboot/web/HelloControllerTest.java
@@ -1,9 +1,13 @@
 package com.wandu.book.springboot.web;
 
+import com.wandu.book.springboot.config.auth.SecurityConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -14,13 +18,19 @@ import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @RunWith(SpringRunner.class) //테스트 진행시 JUnit 내장 실행자 외에 다른 실행자 실행. 연결하는 역할을 함.(여기서는 SpringRunner 실행)
-@WebMvcTest(controllers = HelloController.class)
+@WebMvcTest(controllers = HelloController.class,
+    excludeFilters = {
+        @ComponentScan.Filter(type= FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
+            //@WebMvcTest는 @ControllerAdvice와 @Controller를 읽고 
+            // @Repository, @Service, @Component는 읽지 않으므로 오류를 없애기 위해 스캔 대상에서 SecutiryConfig를 제거함
+    })
     // 여러 스프링 테스트 어노테이션 중 Web에 집중할 수 있는 어노테이션. Controller, ControllerAdvice 사용 가능.
     // 단 @Service, @Component, @Repository 등은 사용 불가
 public class HelloControllerTest {
     @Autowired //스프링이 관리하는 빈을 주입받음
     private MockMvc mvc; //웹API를 테스트할 때 사용. 스프링 MVC 테스트의 시작.
 
+    @WithMockUser(roles = "USER")
     @Test
     public void hello가_리턴() throws Exception {
         String hello = "hello";
@@ -31,6 +41,7 @@ public class HelloControllerTest {
                     //mvc.perform의 결과 검증. 응답 본문의 내용을 검증. 여기선 "hello" 리턴하므로 이 값이 맞는지 검증.
     }
 
+    @WithMockUser(roles = "USER")
     @Test
     public void helloDto가_리턴() throws Exception {
         String name = "hello";
